a08cf35507b4dbd982a8b64f1e34a8295cbe0d59
enzodevs2000 <enzodevs2000@gmail.com>
2022-07-12 17:06:25 -0300

    initial setup finished

---
+++ projetos/dsmeta/backend/src/test/java/com/enzodevs2000/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enzodevs2000.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
