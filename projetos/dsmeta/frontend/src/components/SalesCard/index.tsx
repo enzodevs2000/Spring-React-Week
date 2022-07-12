df69d7d8df82f236c7b2eef67348a0f13b15049c
enzodevs2000 <enzodevs2000@gmail.com>
2022-07-12 18:16:05 -0300

    Datepicker

--- projetos/dsmeta/frontend/src/components/SalesCard/index.tsx
+++ projetos/dsmeta/frontend/src/components/SalesCard/index.tsx
@@ -1,86 +1,96 @@
-import NotificationButton from '../NotificationButton'
+import NotificationButton from "../NotificationButton";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
-import './style.css';
+import "./style.css";
 
 function SalesCard() {
   return (
     <>
       <div className="dsmeta-card">
-                        <h3>Vendas</h3>
-                        <div>
-                            <div className="dsmeta-input-container">
-                                <input className="dsmeta-form-input" type="text"/>
-                            </div>
+        <h3>Vendas</h3>
+        <div>
+          <div className="dsmeta-input-container">
+            <DatePicker
+              selected={new Date()}
+              onChange={(date: Date) => {}}
+              className="dsmeta-form-input"
+              dateFormat="dd/MM/yyyy"
+            />
+          </div>
 
-                            <div className="dsmeta-input-container">
-                                <input className="dsmeta-form-input" type="text"/>
-                            </div>
+          <div className="dsmeta-input-container">
+            <DatePicker
+              selected={new Date()}
+              onChange={(date: Date) => {}}
+              className="dsmeta-form-input"
+              dateFormat="dd/MM/yyyy"
+            />
+          </div>
+        </div>
 
-                        </div>
+        <div>
+          <table className="dsmeta-sales-table">
+            <thead>
+              <tr>
+                <th className="show992">Id</th>
+                <th className="show576">Data</th>
+                <th>Vendedor</th>
+                <th className="show992">Visitas</th>
+                <th className="show992">Vendas</th>
+                <th>Total</th>
+                <th>Notificar</th>
+              </tr>
+            </thead>
 
-                        <div>
-                            <table className="dsmeta-sales-table">
-                                <thead>
-                                    <tr>
-                                        <th className="show992">Id</th>
-                                        <th className="show576">Data</th>
-                                        <th>Vendedor</th>
-                                        <th className="show992">Visitas</th>
-                                        <th className="show992">Vendas</th>
-                                        <th>Total</th>
-                                        <th>Notificar</th>
-                                    </tr>
-                                </thead>
+            <tbody>
+              <tr>
+                <td className="show992">#200</td>
+                <td className="show576">01/02/2000</td>
+                <td>Enzo</td>
+                <td className="show992">10</td>
+                <td className="show992">20</td>
+                <td>R$ 2300,00</td>
+                <td>
+                  <div className="dsmeta-red-button-container">
+                    <NotificationButton />
+                  </div>
+                </td>
+              </tr>
 
-                                <tbody>
-                                    <tr>
-                                        <td className="show992">#200</td>
-                                        <td className="show576">01/02/2000</td>
-                                        <td>Enzo</td>
-                                        <td className="show992">10</td>
-                                        <td className="show992">20</td>
-                                        <td>R$ 2300,00</td>
-                                        <td>
-                                            <div className="dsmeta-red-button-container">
-                                                <NotificationButton />
-                                            </div>
-                                        </td>
-                                    </tr>
+              <tr>
+                <td className="show992">#200</td>
+                <td className="show576">01/02/2000</td>
+                <td>Enzo</td>
+                <td className="show992">10</td>
+                <td className="show992">20</td>
+                <td>R$ 2300,00</td>
+                <td>
+                  <div className="dsmeta-red-button-container">
+                    <NotificationButton />
+                  </div>
+                </td>
+              </tr>
 
-                                    <tr>
-                                        <td className="show992">#200</td>
-                                        <td className="show576">01/02/2000</td>
-                                        <td>Enzo</td>
-                                        <td className="show992">10</td>
-                                        <td className="show992">20</td>
-                                        <td>R$ 2300,00</td>
-                                        <td>
-                                            <div className="dsmeta-red-button-container">
-                                                <NotificationButton />
-                                            </div>
-                                        </td>
-                                    </tr>
-
-                                    <tr>
-                                        <td className="show992">#200</td>
-                                        <td className="show576">01/02/2000</td>
-                                        <td>Enzo</td>
-                                        <td className="show992">10</td>
-                                        <td className="show992">20</td>
-                                        <td>R$ 2300,00</td>
-                                        <td>
-                                            <div className="dsmeta-red-button-container">
-                                                <NotificationButton />
-                                            </div>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-
-                    </div>
+              <tr>
+                <td className="show992">#200</td>
+                <td className="show576">01/02/2000</td>
+                <td>Enzo</td>
+                <td className="show992">10</td>
+                <td className="show992">20</td>
+                <td>R$ 2300,00</td>
+                <td>
+                  <div className="dsmeta-red-button-container">
+                    <NotificationButton />
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default SalesCard
+export default SalesCard;
